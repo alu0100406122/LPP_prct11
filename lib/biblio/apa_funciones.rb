c060b2a61a95d289508b82e71acaaaf7f37d9a8c
alu0100406122 <alu0100406122@ull.edu.es>
2015-12-11 14:57:40 +0000

    Modificando funciones.

--- lib/biblio/apa_funciones.rb
+++ lib/biblio/apa_funciones.rb
@@ -18,8 +18,13 @@ class Apa
     def regla_autor(string)
         str=""
         proc = proc_from{|array| array.each_with_index do |word,i|
-                        str += array[i].split(" ")[1]+" "+ array[i].split(" ")[0]+", "
-                    end
+                if (i+1 == array.size) then
+                    str += array[i].split(" ")[1]+" "+ array[i].split(" ")[0]+" "
+                else
+                    str += array[i].split(" ")[1]+" "+ array[i].split(" ")[0]+" & "
+                end
+                i += 1
+            end
         }
         proc.call(string)
         puts str
@@ -54,8 +59,8 @@ class Apa
         ordenar()
         aux = @atributo.inicio
         while(aux != nil)
-            letra_inicial(aux.value.autor)
-            #regla_autor(aux.value.autor)
+            #letra_inicial(aux.value.autor)
+            regla_autor(aux.value.autor)
             regla_titulo(aux.value.titulo)
             aux = aux.next
         end
@@ -64,19 +69,22 @@ class Apa
 end
 
 libro1 = Libro.new(["Dave Thomas","Scott Chacon", "Chad Fowler"], "programming ruby: the pragmatic programmers guide.", "Pragmatic Bookshelf", "", 4, "(July 7, 2013)", ["ISBN-13: 978-1937785499", "ISBN-10: 1937785491"])
-libro1 = Libro.new(["Hunt Flanagan"], "pro git 2009th edition.", "Apress", "", 2009, "(August 27, 2009)", ["ISBN-13: 978-1430218333","ISBN-10: 1430218339"])
-libro2 = Libro.new(["Andy Aunt"], "pro git 2009th edition.", "Apress", "", 2009, "(August 27, 2009)", ["ISBN-13: 978-1430218333","ISBN-10: 1430218339"])
+libro2 = Libro.new(["Hunt Flanagan"], "pro git 2009th edition.", "Apress", "", 2009, "(August 27, 2009)", ["ISBN-13: 978-1430218333","ISBN-10: 1430218339"])
+libro3 = Libro.new(["Andy Aunt"], "pro git 2009th edition.", "Apress", "", 2009, "(August 27, 2009)", ["ISBN-13: 978-1430218333","ISBN-10: 1430218339"])
 
 revista1 = Revista.new(["Thomas R."], "ruby guide.", "Pragmatic Bookshelf", "", 6, "(July 4, 2012)", ["ISSN-13: 95499"])
 
 nodo1 = Node.new(libro1)
 nodo2 = Node.new(libro2)
-nodo3 = Node.new(revista1)
+nodo3 = Node.new(libro3)
+nodo4 = Node.new(revista1)
 
 apa = Apa.new()
 
 apa.atributo.insertar(nodo1)
 apa.atributo.insertar(nodo2)
+apa.atributo.insertar(nodo3)
+apa.atributo.insertar(nodo4)
 
 apa.to_s()
 
